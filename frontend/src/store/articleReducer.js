5beb262da6a401ab7507aa2176c3c62340cb1fd0
YinYang117 <slcryan2010@yahoo.com>
2022-03-30 10:45:38 -0600

    phase 1

---
+++ frontend/src/store/articleReducer.js
@@ -0,0 +1,39 @@
+import articles from '../data/data.json';
+
+const LOAD_ARTICLES = 'article/loadArticles';
+const ADD_ARTICLE = 'article/addArticle';
+
+export const fetchArticles = () => async dispatch => {
+  const response = await fetch('/api/articles');
+  const articles = await response.json();
+  dispatch(loadArticles(articles));
+}
+
+export const loadArticles = () => {
+  return {
+    type: LOAD_ARTICLES,
+    articles
+  };
+};
+
+export const addArticle = (article) => {
+  return {
+    type: ADD_ARTICLE,
+    article
+  };
+};
+
+const initialState = { entries: [], isLoading: true };
+
+const articleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_ARTICLES:
+      return { ...state, entries: [...action.articles] };
+    case ADD_ARTICLE:
+      return { ...state, entries: [...state.entries, action.article] };
+    default:
+      return state;
+  }
+};
+
+export default articleReducer;
